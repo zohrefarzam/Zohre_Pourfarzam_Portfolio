8042246273f54f023da1a2e1191b8b523b8674f7
zohre farzam <zohrefarzam@gmail.com>
2026-06-01 14:02:58 +0330

    Fix base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/Zohre_Pourfarzam_Portfolio/",
   plugins: [react(), tailwindcss()],
 });
